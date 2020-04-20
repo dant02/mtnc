59aa269b657bfd48230ac7baf63c9ab1db6c4d0c
Daniel Trubač <40642559+dant02@users.noreply.github.com>
2020-04-21 00:44:57 +0200

    fb3 variables

--- db-fb/make.bat
+++ db-fb/make.bat
@@ -1,11 +1,10 @@
 :: make db and its structure, expects firebird 2.5.x installed in c:\fb
 :: https://stackoverflow.com/questions/1601947/how-do-i-create-a-new-firebird-database-from-the-command-line
 
+call .\variables.bat
+
 del d:\fbdata\mtnc.fdb
 
-SET isql=c:\fb\bin\isql.exe
-
-::%isql% -input mtnc.base.sql
 %isql% -b -e -q -charset UTF8 -u SYSDBA -p masterkey -i mtnc.base.sql
 
 ::%isql% -b -e -q -charset UTF8 -u SYSDBA -p masterkey -i mtnc.objects.sql "d:\fbdata\mtnc.fdb"
